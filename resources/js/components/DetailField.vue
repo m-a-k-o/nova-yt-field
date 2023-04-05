abbfd637c17b4fbc56ae6be3cf1c415a61aa6448
Mako <marek@racik.info>
2023-04-05 13:56:21 +0200

    Init

---
+++ resources/js/components/DetailField.vue
@@ -0,0 +1,15 @@
+<template>
+    <PanelItem :index="index" :field="field">
+        <template v-if="field.value" #value>
+            <iframe id="ytplayer" type="text/html" width="100%" height="400" class="rounded"
+                    :src="'https://www.youtube.com/embed/' + field.value"
+                    frameborder="0"></iframe>
+        </template>
+    </PanelItem>
+</template>
+
+<script>
+export default {
+  props: ['index', 'resource', 'resourceName', 'resourceId', 'field'],
+}
+</script>
---
+++ resources/js/components/IndexField.vue
@@ -0,0 +1,18 @@
+<template>
+    <span>
+        <img v-if="fieldValue" :src="'https://img.youtube.com/vi/' + fieldValue + '/default.jpg'" style="object-fit: cover;" class="rounded-full w-8 h-8" alt="YouTube Image Preview" />
+        <span v-else>&mdash;</span>
+    </span>
+</template>
+
+<script>
+export default {
+  props: ['resourceName', 'field'],
+
+  computed: {
+    fieldValue() {
+      return this.field.displayedAs || this.field.value
+    },
+  }
+}
+</script>
